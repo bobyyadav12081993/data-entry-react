0510ed312123a365bb2b2d9afcec9affc2fa8294
Boby Yadav <bobyyadav12081993@gmail.com>
2020-05-17 19:21:02 +0530

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.scss";
-// import Navbar from "./components/header/header.component";
 import Navbar from "./container/header-container";
-// import DataEntry from './components/data-entry/data-entry.component';
 import DataEntry from "./container/data-entry-container";
 import { changeBodyTheme } from "./services/utility";
 
@@ -17,20 +15,6 @@ class App extends React.Component {
         <div className="container-fluid">
           <DataEntry></DataEntry>
         </div>
-
-        {/* <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'>
-            Learn React
-          </a>
-        </header> */}
       </div>
     );
   }
